065144563bb409a81e738a2c847c252e8340d879
Whodarshanpatel <137098128+Whodarshanpatel@users.noreply.github.com>
2025-12-16 05:54:02 +0000

    save changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId =1332
+let accountEmail ="darshanbpatel228599@gmail.com"
+var accountPassword="1234"
+accountCity="Vadodara"
+
+// accountId =222
+accountEmail="sdf@gmail.com"
+console.log(accountId);
+console.log(accountEmail)
+accountPassword="1111"
+console.log(accountPassword)
+console.table([accountEmail,accountId,accountPassword,accountCity])
+// prefer not to use var as it has problem in scope
+let accountState;
+console.log(accountState)
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict";
+// treat all js code as newer version
+// alert("hello how are ?")
+// alert(3+4)
+// we are using node js not browser
+console.log(3+4) ;
+console.log('hello')
