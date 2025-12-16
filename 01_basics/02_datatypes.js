f543d2669bf018699911dda1d335193f2e942853
Whodarshanpatel <137098128+Whodarshanpatel@users.noreply.github.com>
2025-12-16 06:06:40 +0000

    done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,7 +1,23 @@
-"use strict";
+
 // treat all js code as newer version
 // alert("hello how are ?")
 // alert(3+4)
 // we are using node js not browser
 console.log(3+4) ;
-console.log('hello')
+// console.log('hello')
+let name = "hitesh"
+// let age =18
+let isLoggedIn=false
+// number => 2 to the power 53
+// bigint
+// string=>""
+// boolean => true/false
+// null => standalone value (empty)
+// undefined => 
+// symbol : to make variable is unique 
+
+
+// object 
+
+console.log(typeof null) // object
+console.log(typeof(undefined))//undefined
